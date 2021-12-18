943f2ac741d72cb97370e5f44aa5b3ad62c79344
Roshan Thapa <roshan.thapa85@yahoo.com>
2021-12-18 19:48:31 +0545

    changed app.js in js folder

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ const MessageBox = document.querySelector('#MessageBox')
 const LocationBox = document.querySelector('#LocationBox')
 
 
-const fetchFunc = (address) => { fetch("http://localhost:3000/weather?address=" + address).then((response)=>{
+const fetchFunc = (address) => { fetch("/weather?address=" + address).then((response)=>{
     response.json().then((data)=>{
         
         if (data.error)
